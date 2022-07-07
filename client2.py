4e2446552dba96f088e44dae1820434cd202b527
vintello <vintello@ukr.net>
2022-07-07 16:12:46 +0300

    read from serial

--- client2.py
+++ client2.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-
-# WS client example
-
 import asyncio
 import json
+import serial
 
 import websockets
 import random
 
 async def hello():
+    sdata = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1.0)
+    await asyncio.sleep(2)
+    sdata.reset_input_buffer()
+    print("Arduino is Connected")
     async with websockets.connect('ws://localhost:8480/ws') as websocket:
         while True:
-            name = random.randrange(1,1000, 4)
-            name = json.dumps({"action": "data", "data": name})
-            await websocket.send(name)
-            print(f"> {name}")
-            await asyncio.sleep(2)
-
-        #greeting = await websocket.recv()
-        #print(f"< {greeting}")
+            if sdata.in_waiting > 0:
+                mydata = sdata.readline().decode("utf-8").rstrip()
+                print(mydata)
+                name = json.dumps({"action": "data", "data": mydata})
+                await websocket.send(name)
+            await asyncio.sleep(0.1)
 
 asyncio.get_event_loop().run_until_complete(hello())
